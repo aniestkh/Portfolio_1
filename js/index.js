16d55e0878215e86b000d2fb0e1d54fec4976105
aniestkh <108396590+aniestkh@users.noreply.github.com>
2022-09-16 14:51:38 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -47,11 +47,11 @@ $('body').on('click', '.closenav', function(){
     $('.side').css({display:'none'})
 })
 
-// 로고 -> 화면사이즈 
-//$('#header .mainlogo').on('click', function(){
-//   var wid=$(window).width()
-//    var hei=$(window).height()
-//    alert(wid)
-//    alert(hei)
-//})
+// test)핸드폰 가로 세로 크기 구하기
+// $('#header .mainlogo').on('click', function(){
+//     var wid=$(window).width()
+//     var hei=$(window).height()
+//     alert(wid)
+//     alert(hei)
 
+// })
